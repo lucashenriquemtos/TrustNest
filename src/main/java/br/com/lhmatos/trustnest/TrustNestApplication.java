e2a65f33d7e6a39025fa3fa707b1fdc51eb8aa37
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2023-08-30 14:07:19 -0300

    Initial Commit

---
+++ src/main/java/br/com/lhmatos/trustnest/TrustNestApplication.java
@@ -0,0 +1,13 @@
+package br.com.lhmatos.trustnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lhmatos/trustnest/TrustNestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lhmatos.trustnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
