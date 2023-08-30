380a9bb87e7936f2bacf5fe9fc796c47c323ac2b
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2023-08-30 14:37:30 -0300

    Inicializando DotEnv

--- src/main/java/br/com/lhmatos/trustnest/TrustNestApplication.java
+++ src/main/java/br/com/lhmatos/trustnest/TrustNestApplication.java
@@ -1,5 +1,6 @@
 package br.com.lhmatos.trustnest;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TrustNestApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
 		SpringApplication.run(TrustNestApplication.class, args);
 	}
 
